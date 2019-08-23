6a1c97fafad60b0a92b030c9a0a798588d0a39f6
examknow <54456226+examknow@users.noreply.github.com>
2019-08-23 17:26:51 -0500

    Create GlobalCheckUser.alias.php

---
+++ GlobalCheckUser.alias.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Aliases for Special:GlobalCheckUser
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = [];
+
+/** English (English) */
+$specialPageAliases['en'] = [
+	'GlobalCheckUser' => [ 'GlobalCheckUser' ],
+	'GlobalCheckUserLog' => [ 'GlobalCheckUserLog' ],
+];
